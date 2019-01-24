ca870de17d79328f7b5e35162389011729431484
Dilan <dilanpace1496@gmail.com>
2019-01-23 22:22:28 -0500

    test

--- path.js
+++ path.js
@@ -1,31 +1,40 @@
 function Particle(x, y) {
-    this.x = x;
-    this.y = y;
-    
-    this.history = [];
-    
-    this.update = function() {
-      
-      
-  
-      var v = createVector(this.x, this.y);
-      this.history.push(v); 
-      
-    }
-    
-    this.show = function() {
-      
-      
-      
-      beginShape(POINTS);
-      for (var i = 0; i < this.history.length; i++) {
-        var pos = this.history[i];
-        
-        vertex(pos.x, pos.y);
-      }
-      endShape(CLOSE);
-      
-      
-    }
-    
+  this.x = x;
+  this.y = y;
+
+  this.history = [];
+
+  this.update = function () {
+
+
+
+    var v = createVector(this.x, this.y);
+    this.history.push(v);
+
   }
+
+  this.show = function () {
+
+
+
+    beginShape(POINTS);
+    for (var i = 0; i < this.history.length; i++) {
+      var pos = this.history[i];
+
+      vertex(pos.x, pos.y);
+    }
+    endShape(CLOSE);
+
+
+  }
+
+  beginShape();
+  vertex(220, 220);
+  vertex(240, 220);
+  vertex(240, 240);
+  vertex(260, 240);
+  vertex(260, 260);
+  vertex(220, 260);
+  endShape(CLOSE);
+
+}
